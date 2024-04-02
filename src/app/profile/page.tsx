7edd70eff90f94b67039ca6e6aad441c2dae5bcd
Animesh Yadav <animeshy068@gmail.com>
2024-04-02 13:57:00 +0530

    Added

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function profilePage(){
+    return(
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen py-3">
+                <h1>This is Profile page</h1>
+            </div>
+        </>
+    );
+}
